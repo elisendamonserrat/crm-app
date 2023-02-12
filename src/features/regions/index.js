5670a5b2656bf2441e615fc064bbe536ae33546b
elisenda <elisenda.monserrat@gmail.com>
2023-02-12 07:31:44 +0100

    feat: creates src folder and main subfolders

---
+++ src/features/regions/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import Title from "../../../components/Title";
+import Button from "../../../components/Button";
+
+const List = () => {
+    const { navigate } = useNavigation();
+    
+    // Select store state that holds the regions list
+    // Loop through the list
+
+  return (
+    <View>
+      <Title text="Regions List" />
+
+      <Button
+        text="X Region Name"
+        onPress={() => {
+          navigate("Users by Region", { region: region });
+        }}
+        disabled={false}
+      />
+    </View>
+  );
+};
+
+export default List;
